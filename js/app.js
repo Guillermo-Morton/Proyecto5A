7d25ac82f91cbd32d442a4295bfdcf9528393b59
Guillermo Morton <guillermort2000@gmail.com>
2021-03-15 14:43:45 -0300

    Correccion de bugs en la pagina de inicio debido a la funcion destacada

--- js/app.js
+++ js/app.js
@@ -1,20 +1,23 @@
+// Inicializamos las variables arrays
 let categoriaAccion = [];
 let categoriaDrama = [];
 let categoriaComedia= [];
 let categoriaRomance= [];
 let listaPelicula= [];
 let peliculaDestacada= [];
+
 leerPeliculas();
 cargarPelicula();
 cargarDestacada();
-// cargarDestacada();
-console.log(peliculaDestacada);
 
 
+
+// Esta funcion va a leer los datos existentes en el localstorage
 function leerPeliculas(){
-    // Esta funcion va a leer los datos existentes en el localstorage
+    // Si detecta que existen datos en el localstorage, los extrae a un array y los filtra segun la categoria
     if(localStorage.length > 0){
         listaPelicula = JSON.parse(localStorage.getItem('listaPeliculasKey'));
+        // este array recibe los datos de la pelicula destacada
         peliculaDestacada = JSON.parse(localStorage.getItem('peliculaDestacadaKey'));
 
         for(let i in listaPelicula){
@@ -43,8 +46,12 @@ function leerPeliculas(){
             }
         }
         dibujarCard();
+    }else{
+        // si no existe nada en localstorage, el array de la pelicula destacada es igual a null
+        peliculaDestacada = null;
     }
 }
+// Esta funcion se encarga de pintar las peliculas que carguemos al localstorage segun la categoria
 function dibujarCard(){
     let infoPelicula='';
     let img='';
@@ -58,6 +65,7 @@ function dibujarCard(){
     romance.innerHTML='';
     let codigo='';
             for(let i in categoriaAccion){
+                // si no se ecuentra una imagen
                 if(categoriaAccion[i].imagen===''){
                     // se agrega una imagen por defecto
                     console.log('se agrego una imagen por defecto');
@@ -71,11 +79,12 @@ function dibujarCard(){
                     <a href="#"><img src="${img}" class="w-100" alt=""></a>
                 </div>
                 `;
-                // agregar las peliculas al carrete
+                // agregar las peliculas de accion al carrete
         
                 accion.innerHTML += infoPelicula;
             }
             for(let i in categoriaDrama){
+                // si no se ecuentra una imagen
                 if(categoriaDrama[i].imagen===''){
                     // se agrega una imagen por defecto
                     console.log('se agrego una imagen por defecto');
@@ -90,11 +99,12 @@ function dibujarCard(){
                     <a href="#"><img src="${img}" class="w-100" alt=""></a>
                 </div>
                 `;
-                // agregar las peliculas al carrete
+                // agregar las peliculas de drama al carrete
         
                 drama.innerHTML += infoPelicula;
             }
             for(let i in categoriaComedia){
+                // si no se ecuentra una imagen
                 if(categoriaComedia[i].imagen===''){
                     // se agrega una imagen por defecto
                     console.log('se agrego una imagen por defecto');
@@ -108,11 +118,12 @@ function dibujarCard(){
                     <a href="#"><img src="${img}" class="w-100" alt=""></a>
                 </div>
                 `;
-                // agregar las peliculas al carrete
+                // agregar las peliculas de comedia al carrete
         
                 comedia.innerHTML += infoPelicula;
             }
             for(let i in categoriaRomance){
+                // si no se ecuentra una imagen
                 if(categoriaRomance[i].imagen===''){
                     // se agrega una imagen por defecto
                     console.log('se agrego una imagen por defecto');
@@ -126,11 +137,12 @@ function dibujarCard(){
                     <a href="#"><img src="${img}" class="w-100" alt=""></a>
                 </div>
                 `;
-                // agregar las peliculas al carrete
+                // agregar las peliculas de romance al carrete
         
                 romance.innerHTML += infoPelicula;
             }
 }
+// esta funcion se encarga de reemplazar la imagen de la pelicula en la pagina de pelicula, por un trailer reproducible
 let main=document.getElementById('main');
 let pelicula=[];
 let titulo='';
@@ -147,26 +159,29 @@ function reproducir(){
     </section>
     `
 }
+// esta funcion se encarga de filtrar la pelicula seleccionada del carrete para que expanda los detalles de dicha pelicula
 function cargarPelicula(){
     let id=''; 
     let elemento = document.getElementsByClassName('pelicula');
+    // extraemos el id del elemento al que hacemos click
     for(let i=0; i<elemento.length;i++){
         elemento[i].addEventListener('click', function(){
             id=this.id;
             console.log(id);
-  
+            // filtramos los datos del objeto segun el id del elemento al que clickeamos
             for(let i in listaPelicula){
                 if(listaPelicula[i].codigo==id){
                     console.log('pelicula filtrada');
+                    // los pasamos a un array
                     pelicula.push(listaPelicula[i]);
                     console.log(pelicula);
-                    
+                    // asignamos la informacion a variables
                     titulo=pelicula[0].nombre;
                     portada=`img/peliculas/${pelicula[0].imagen}`;
                     descripcion=pelicula[0].descripcion;
                     let video='';
  
-
+                    // reemplazamos el codigo
                     main.innerHTML= `
                     <h1 class="mx-5 mt-5 mb-4 display-3">${titulo}</h1>
                     <img class="w-100 destacada" src="${portada}" alt="imagen de pelicula">
@@ -182,14 +197,17 @@ function cargarPelicula(){
         }
     )}
 }
-
+// estas tres funciones existen para ejecutar correctamente la funcionalidad de destacar peliculas
 function cargarDestacada(){
     let destacada=document.getElementById('portada');
     console.log(destacada);
+    // si nuestro array es distinto de null
     if(peliculaDestacada!=null){
+        // inicializamos variables asignando la unformacion de la pelicula destacada guardada en localstorage
         let portada=`img/peliculas/${peliculaDestacada[0].imagen}`;
         let titulo=peliculaDestacada[0].nombre;
         let descripcion=peliculaDestacada[0].descripcion;
+        // reemplazamos el codigo de la portada con la informacion correspondiente
             destacada.innerHTML=`
             <img src="${portada}" class="w-100 destacada" alt="">
             <div class="container textosPortada">
@@ -199,11 +217,12 @@ function cargarDestacada(){
              </div>
             `         
     }else{
+        // si nuestro array es igual a null, mostrar una portada por defecto con el error 404
         destacada.innerHTML=`
             <img src="img/error404.png" class="mx-auto destacada" alt="">
             <div class="container textosPortada">
                  <h2 class="font-weight-bold">No se encontro la pelicula destacada</h2>
-                 <p class="lead ">No existe una descripciongi</p>
+                 <p class="lead ">No existe una descripcion</p>
                  <a onclick='paginaDestacada()' class="btn boton px-5 disabled" href="#">Reproducir</a>
              </div>
             `         
